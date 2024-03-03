ed0980cf53b7bbbab6ba5e141a4b3c3f8d6e82c0
Lokesh Kharkwal <115694554+lokeshkharkwal7@users.noreply.github.com>
2024-03-03 22:30:39 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,11 @@ let cors = require('cors')
 
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+  origin: ["https://deploy-vercelEcommerce-1whq.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  // credentials: true
+}))
 
 // importing config files
 const mongoConnect = require("./connect");
