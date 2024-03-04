b355fc1d91cd2d783d19f5fa0855f0e04695ad2e
Lokesh Kharkwal <115694554+lokeshkharkwal7@users.noreply.github.com>
2024-03-05 00:39:42 +0530

    Update AdminProductEditCard.js

--- frontend/src/components/Admin/AdminProductEditCard.js
+++ frontend/src/components/Admin/AdminProductEditCard.js
@@ -59,7 +59,7 @@ function AdminProductEditCard({
     <div>
       <div
         className="card-fluid text-secondary bg-light mx-3 "
-        style={{ width: "15rem" }}
+        style={{ width: "25rem", height: "40rem" }}
       >
         <img src={pimages} className="card-img-top img-responsive" alt="..." />
         <div className="card-body">
