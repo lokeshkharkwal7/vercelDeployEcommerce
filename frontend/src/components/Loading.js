29b977fe8d2d62c156b256395c644f23444429db
Lokesh Kharkwal <115694554+lokeshkharkwal7@users.noreply.github.com>
2024-03-05 00:32:35 +0530

    Add files via upload

---
+++ frontend/src/components/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Loading() {
+  return (
+    <div>
+      <div class="spinner-grow" role="status">
+  <span className="sr-only">Loading...</span>
+</div>
+    </div>
+  )
+}
+
+export default Loading
