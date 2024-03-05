37fb86a116ad76b413a84161f1ffdea1ea6df595
Lokesh Kharkwal <115694554+lokeshkharkwal7@users.noreply.github.com>
2024-03-05 17:26:49 +0530

    Update Home.js

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { fetchProducts } from "../ProjectAPIS/fetchProduct";
 import ProductCard from "./ProductCard";
 import { useDispatch, useSelector } from "react-redux";
@@ -6,12 +6,26 @@ import { fetchUser } from "../ProjectAPIS/fetchUser";
 import Navbar from "./Navbar";
 import { fetchCartProduct } from "../ProjectAPIS/fetchCart";
 import Loading from "./Loading";
+import AlertMessage from "./AlertMessage";
 
 function Home({ category = "Electronics" }) {
   const dispatch = useDispatch();
   const authToken = localStorage.getItem("user_auth_token");
 
   console.log("User token get from the Login page is : ", authToken);
+  const [addToCartStatus, setAddToCarStatus] = useState(false);
+
+  // for showing alert messages when data successfully entered to the cart
+  const getAddToCartStatus = (params) => {
+    setAddToCarStatus(params);
+  };
+
+  // this code will reset the add to cart status to false and since it is messing with the code we have set timeout to 1 sec for perfect execution
+  const changeCartStatusToFalse = () => {
+    setTimeout(() => {
+      setAddToCarStatus(false);
+    }, 1000);
+  };
 
   useEffect(() => {
     fetchProducts(authToken, category, dispatch);
@@ -29,9 +43,13 @@ function Home({ category = "Electronics" }) {
       <Navbar />
       <br />
       <br />
+
       {/* using loading */}
       {products.length === 0 ? (
-        <div> <Loading /> </div>
+        <div>
+          {" "}
+          <Loading />{" "}
+        </div>
       ) : (
         <div className="container-fluid fs-4 text-center bg-primary text-light mt-2">
           <i className="fa-solid fa-layer-group fs-6 mx-2"></i>
@@ -39,10 +57,21 @@ function Home({ category = "Electronics" }) {
         </div>
       )}
 
+      <AlertMessage message={"Welcome Back"} />
+      {addToCartStatus === false ? (
+        ""
+      ) : (
+        <div>
+          <AlertMessage message={addToCartStatus} />
+          {changeCartStatusToFalse()}
+        </div>
+      )}
+
       <div className="container-fluid d-flex flex-wrap mt-3">
         {products.map((content) => (
           <ProductCard
             key={content.pdescription}
+            addToCartStatus={getAddToCartStatus}
             pname={content.pname}
             pprice={content.pprice}
             pimages={content.pimages}
