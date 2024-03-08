26a2accdbccf9eebb42a4232a444baac4b15e836
Lokesh Kharkwal <115694554+lokeshkharkwal7@users.noreply.github.com>
2024-03-08 21:26:28 +0530

    Update connect.js
    Making Repo Ready to deploy

--- backend/connect.js
+++ backend/connect.js
@@ -1,19 +1,3 @@
-// //CONNECTING TO THE MONGOOSE SERVER
-// const mongoose = require('mongoose')
-// const uri = "mongodb+srv://lokeshkharkwal:lokeshkharkwal@cluster0.ntpuvkr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-// try {
-//     mongoose.connect(uri).then(
-//     console.log('Connected to the Database'))
-// } catch (error) {
-//     console.log(error)
-    
-// }
-
-
-// // mongoose.connect(uri)
-// // .then("console.log("You are connected")
-// // .catch(err => console.log(err.reason));
-
 const mongoose = require('mongoose');
 const uri = "mongodb+srv://lokeshkharkwal:lokeshkharkwal@cluster0.ntpuvkr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
